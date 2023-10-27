2fe30b9b2c88573ebf694105509cd70a6acc876c
John Michael <johnny25580@gmail.com>
2023-10-27 21:11:00 +0100

    revoken commit

--- src/main/java/com/johnny/profileservice/domain/RevokeToken.java
+++ src/main/java/com/johnny/profileservice/domain/RevokeToken.java
@@ -1,2 +1,34 @@
-package com.johnny.profileservice.domain;public class RevokeToken {
+package com.johnny.profileservice.domain;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.Instant;
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Table(name = "revoke_token")
+public class RevokeToken {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @NotNull
+    @Column(name = "token", nullable = false, unique = true)
+    private String token;
+
+    @NotNull
+    @Column(name = "expiry_date", nullable = false)
+    private Instant expiryDate;
+    @ManyToOne
+    @JsonIgnoreProperties(value = { "assignedOrgs", "authorities", "refreshTokens", "revokedTokens" }, allowSetters = true)
+    private AppUser appUser;
 }
